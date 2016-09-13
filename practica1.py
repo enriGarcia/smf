8d73863e2fe04338c84428a2af1bc9271dfaabb2
enriGarcia <phd.enrique.garcia@ieee.org>
2016-09-13 15:27:39 -0500

    Add files via upload
    Práctica 1. Sistemas de Manufactura Flexible

---
+++ practica1.py
@@ -0,0 +1,4 @@
+nombre=raw_input('Ingresa tu nombre: ')
+edad=int(raw_input('Ingresa tu edad: '))
+estatura=float(raw_input('Ingresa tu estatura: '))
+print 'Eres', nombre, 'y naciste en', 2016-edad, 'y mides', estatura
